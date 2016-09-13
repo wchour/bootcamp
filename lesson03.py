881b37137f849c3bf782fe8f4acbf705a1a9d6c6
William Chour <wchour@gmail.com>
2016-09-13 13:31:08 -0700

    added more

--- lesson03.py
+++ lesson03.py
@@ -14,3 +14,31 @@ def xa_to_diameter(xa):
     diameter = np.sqrt(xa *4/np.pi)
 
     return diameter
+
+A = np.array([[6.7, 1.3, 0.6, 0.7],
+              [0.1, 5.5, 0.4, 2.4],
+              [1.1, 0.8, 4.5, 1.7],
+              [0.0, 1.5, 3.4, 7.5]])
+
+b = np.array([1.1, 2.3, 3.3, 3.9])
+
+# Print row 1 of A
+# return A[1,:]
+
+# Print columns 1 & 3 of A
+# return A[:,[1,3]]
+
+# Print values of every entry in A that is greater than 2.
+# A[A > 2]
+
+# Ax = b
+
+def easy_reshape(obj, ncols, order = 'C'):
+    '''
+    Guarantee that reshaping works by defining only one shape parameter.
+    '''
+
+    # Reshape
+    arr = np.reshape(obj, (ncols, len(obj)/ncols), order)
+
+    return arr
--- plotting_round_one.py
+++ plotting_round_one.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Set matplotlib rc params.
+
+rc = {'lines.linewidth' : 2, 'axes.labelsize' : 18, 'axes.titlesize' : 18}
+sns.set(rc=rc)
+
 # load the food data
 xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
 xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
@@ -12,6 +17,6 @@ bins = np.arange(1700, 2500, 50)
 
 # Plot the data as a histogram
 _ = plt.hist(xa_low, bins=bins)
-plt.xlabel('Cross-sectional area(μm$^2$)')
-plt.ylabel('count')
+plt.xlabel('Cross-sectional area(μm$^2$)', fontsize=18)
+plt.ylabel('count', fontsize=18, rotation='vertical')
 plt.show()
